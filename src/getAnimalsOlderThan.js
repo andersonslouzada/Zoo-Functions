66dd22ac71a2e63274a7f54545323958df6a23df
andersonlouzada <anderson.s.louzada@gmail.com>
2022-12-20 12:07:49 -0300

    cria função do requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalName = species.find((element) => element.name === animal);
+  return animalName.residents.every((element) => element.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
