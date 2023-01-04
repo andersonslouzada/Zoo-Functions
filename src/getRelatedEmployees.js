ef4b9054395ee340d1b2a2cfde662486f79e7667
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-04 14:20:54 -0300

    corrige erro do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,7 +4,7 @@ const isManager = (id) => data.employees.some(({ managers }) => managers.include
 
 const getRelatedEmployees = (managerId) => {
   if (!isManager(managerId)) {
-    throw new Error('O id inserido não é de uma  pessoa colaboradora gerente!');
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const workers = data.employees.filter(({ managers }) => managers.includes(managerId));
   return workers.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
