e20a67c545eeb9622ddd18508005d1aa991658af
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-04 15:04:12 -0300

    cria função do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((accumulator, current) => {
+      accumulator[current.name] = current.residents.length;
+      return accumulator;
+    }, {});
+  }
+  const findSpecie = data.species.find((element) => animal.species === element.name);
+  if (animal.sex) {
+    return findSpecie.residents.filter((element) => element.sex === animal.sex).length;
+  }
+  return findSpecie.residents.length;
 };
 
 module.exports = countAnimals;
