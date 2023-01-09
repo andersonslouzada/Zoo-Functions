5855857436f3b013744dc7a031162b8876b0413d
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-09 16:31:10 -0300

    cria função do requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employee = data.employees.find((element) => element.id === id);
+  const firstAnimal = employee.responsibleFor[0];
+  const oldestAnimal = data.species.find((specie) => specie.id === firstAnimal)
+    .residents.reduce((value, element) => (element.age > value.age ? element : value));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
