7f2d243ae0e0374cb9d62839e85f5f3bca084fc0
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-09 17:16:09 -0300

    cria função do requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeesByName = (name) =>
+  data.employees.find((element) => element.firstName === name || element.lastName === name);
+
+const getEmployeesById = (id) => data.employees.find((element) => element.id === id);
+
+const getAnimalById = (id) => data.species.find((element) => element.id === id);
+
+const dataEmployee = (employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: responsibleFor.map((animalId) => getAnimalById(animalId).name),
+    locations: responsibleFor.map((animalId) => getAnimalById(animalId).location),
+  };
+};
+
+const getEmployeesCoverage = (employee) => {
+  if (!employee) return data.employees.map((element) => dataEmployee(element));
+  const { name, id } = employee;
+  const newEmployee = getEmployeesByName(name) || getEmployeesById(id);
+  if (!newEmployee) throw new Error('Informações inválidas');
+  return dataEmployee(newEmployee);
 };
 
 module.exports = getEmployeesCoverage;
