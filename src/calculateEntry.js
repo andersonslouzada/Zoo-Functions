602937a61c6860a46dcd86067e961658f33236ef
andersonlouzada <anderson.s.louzada@gmail.com>
2023-01-09 16:05:09 -0300

    cria funções do requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,31 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
-  // iniciando o projeto
+  let children = 0;
+  let adults = 0;
+  let seniores = 0;
+  Object.values(entrants).forEach((element) => {
+    if (element.age < 18) {
+      children += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      adults += 1;
+    }
+    if (element.age >= 50) {
+      seniores += 1;
+    }
+  });
+  return { child: children, adult: adults, senior: seniores };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) return 0;
+  if (Object.keys(entrants).length === 0) return 0;
+  const visitingPeople = countEntrants(entrants);
+  const totalChildren = visitingPeople.child * data.prices.child;
+  const totalAdults = visitingPeople.adult * data.prices.adult;
+  const totalSeniores = visitingPeople.senior * data.prices.senior;
+  return parseFloat((totalChildren + totalAdults + totalSeniores).toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
